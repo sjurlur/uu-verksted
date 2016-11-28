60fd26b24c550bf611fad1821d5e1751ad1832ab
Sjur Seibt <sjurlur@gmail.com>
2016-11-28 13:41:51 +0100

    legg til fasit

--- src/Skjema/SkjemaOppg.js
+++ src/Skjema/SkjemaOppg.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Skjema.css';
 import Collapse from 'react-collapse';
+import classNames from 'classnames';
 
 class Skjema extends Component {
   constructor(props) {
     super(props);
     this.sendForm = this.sendForm.bind(this);
-    this.state = {sent: false}
+    this.toggleExpand = this.toggleExpand.bind(this);
+    this.state = {sent: false, expanded: false}
   }
   sendForm(evt) {
     console.log(evt);
@@ -15,7 +17,15 @@ class Skjema extends Component {
       this.setState({ sent: false });
     }, 2000);
   }
+  toggleExpand() {
+    const expanded = !this.state.expanded;
+    this.setState({expanded});
+  }
   render() {
+    const buttonClasses = classNames('atom_inline-navigationbutton', 'arrow-after', {
+      'arrow-up': this.state.expanded,
+      'arrow-down': !this.state.expanded,
+    });
     const innsendt = this.state.sent ? 'Skjemaet er innsendt' : null;
     return (
       <div className="page_skjema__oppgave">
@@ -29,7 +39,7 @@ class Skjema extends Component {
             <input type="text" id="etternavn"/>
             <label htmlFor="allergier">Allergier: <em>(må fylles ut)</em></label>
             <input type="text" id="allergier"/>
-            <fieldset>
+            <div className="page_skjema__fieldset">
               <span>Send epost: </span>
               <div>
                 <input type="radio" name="radio" id="ja" value="ja"/>
@@ -39,7 +49,7 @@ class Skjema extends Component {
                 <input type="radio" name="radio" id="Nei" value="Nei"/>
                 <label htmlFor="Nei">Nei</label>
               </div>
-            </fieldset>
+            </div>
             <textarea placeholder="kommentarfelt" />
             <button type="button" onMouseDown={this.sendForm} className="actionbutton">Send</button>
           </form>
@@ -47,7 +57,19 @@ class Skjema extends Component {
             {innsendt}
           </div>
         </div>
-        <h3>Fasit</h3>
+        <section className="page_skjema__fasit">
+          <button type="button" className={buttonClasses} aria-expanded={this.state.expanded} onClick={this.toggleExpand}>{!this.state.expanded ? 'Vis fasit' : 'Skjul fasit'}</button>
+          <Collapse isOpened={this.state.expanded}>
+            <ul>
+              <li>Etikett til etternavn er kodet som <code>&lt;span&gt;</code> i stedet for <code>&lt;label&gt;</code></li>
+              <li>Påkrevd felt allergier mangler attributten <em>required</em></li>
+              <li>Radioknapper er ikke gruppert med <code>&lt;fieldset&gt;</code></li>
+              <li>Etiketten til gruppen med radioknapper er kodet som <code>&lt;span&gt;</code> i stedet for <code>&lt;legend&gt;</code></li>
+              <li>Kommentarfelt mangler etikett</li>
+              <li>Det er bare mulig å klikke på send knappen med musen</li>
+            </ul>
+          </Collapse>
+        </section>
       </div>
     );
   }
