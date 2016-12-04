41f445dc06f9ef8827545863d622c1a0242cf6cf
Sjur Seibt <sjurlur@gmail.com>
2016-12-04 19:05:56 +0100

    skjermleser

--- src/Pages/Skjermleser/Skjermleser.js
+++ src/Pages/Skjermleser/Skjermleser.js
@@ -5,14 +5,16 @@ class Skjermleser extends Component {
     return (
       <div>
        <p>Skjermlesere er dataprogrammer som blir brukt for å lese opp innholdet på skjermen. 
-        De er svært nyttige for testing av tilgjengelighet, da de sier hva elementene faktisk er, og ikke
-        bare hvordan de ser ut. 
+        De er svært nyttige for testing av tilgjengelighet, da de leser opp hva elementene er kodet som. Dermed kan vi se om 
+        sidene er kodet riktig og finne eventuelle utfordringer for brukere som benytter hjelpeteknologi.  
        </p>
       <h2>Hvordan navigere med ChromeVox</h2>
       <ul>
         <li>For å starte ChromeVox. Trykk på <code>ctrl + alt + a + a</code></li>
         <li>Naviger forover med <code>ctrl + alt + pil ned</code></li>
         <li>Naviger bakover med <code>ctrl + alt + pil opp</code></li>
+        <li>"Zoom inn" med <code>ctrl + alt + +</code></li>
+        <li>"Zoom ut" med <code>ctrl + alt + -</code></li>
         <li>For å få den til å være stille, trykk <code>ctrl</code></li>
       </ul>
        <h2>Oppgave</h2>
